8cc4f00a1d5cdb236da212fdb9255dfc8ccb54d9
DragoonKite <frank.depaolo@yahoo.com>
2021-01-13 13:57:47 -0600

    search history now saves/loads using localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,29 @@
-
+var searchHistory = [];
 
 $("button").on("click", function(){
     event.preventDefault();
-    var searchCriteria = document.querySelector("input");
-    console.log(searchCriteria.value.trim());
+    var searchCriteria = document.querySelector("input").value.trim();
+    searchHistory.push(searchCriteria);
+    saveHistory(searchHistory);
     searchCriteria.value = "";
-})
+});
 
+var loadHistory = function(){
+    searchHistory = JSON.parse(localStorage.getItem('searchHistory'));
+    if(!searchHistory){
+        searchHistory = []
+    }
+    else{
+        displayHistory(searchHistory);
+    };
+};
 
+var displayHistory = function(history){
+    console.log(history);
+};
 
+var saveHistory = function(history){
+    localStorage.setItem('searchHistory', JSON.stringify(history))
+};
+
+loadHistory();
